086fcba9ebef2bff60277d5c730d554a0ce54e81
Leimar <leimarferreira100@gmail.com>
2021-04-16 11:14:34 -0300

    Implementing purchase service.

---
+++ src/services/Purchase/index.js
@@ -0,0 +1,31 @@
+import api from '../shared/api';
+
+const getAll = () => {
+    return api.get("/purchase");
+}
+
+const get = id => {
+    return api.get(`/purchase/${id}`);
+}
+
+const create = data => {
+    return api.post("/purchase", data);
+}
+
+const update = (id, data) => {
+    return api.put(`/purchase/${id}`, data);
+}
+
+const remove = id => {
+    return api.delete(`/purchase/${id}`);
+}
+
+const PurchaseDataService = {
+    getAll,
+    get,
+    create,
+    update,
+    remove
+}
+
+export default PurchaseDataService;
