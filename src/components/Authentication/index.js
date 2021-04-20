6abd2d1bf5024637492652fd6591fda5df828dd5
Leimar <leimarferreira100@gmail.com>
2021-04-20 11:31:14 -0300

    Removing unused component from authentication component.

--- src/components/Authentication/index.js
+++ src/components/Authentication/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 import SignIn from './SignIn/index';
 import SignUp from './SignUp/index';
 
