a95706fc19c2f6b020c0d0f7146cc464fef98bee
Leimar <leimarferreira100@gmail.com>
2021-04-20 09:51:16 -0300

    Adding product list to the app.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Header from './Header/index'
 import Sidebar from './Sidebar/index';
+import ProductList from '../Product/List/index';
 
 function App() {
     return (
         <div>
             <Header/>
             <Sidebar/>
+
+            <Switch>
+                <Route exact path={"/product"}>
+                    <ProductList/>
+                </Route>
+            </Switch>
         </div>
     )
 }
