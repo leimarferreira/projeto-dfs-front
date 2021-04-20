7738410bd857591d3fda3b343645cf0382ca2016
Leimar <leimarferreira100@gmail.com>
2021-04-20 09:51:43 -0300

    Updating the sidebar.

--- src/components/App/Sidebar/index.js
+++ src/components/App/Sidebar/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './index.css';
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
-
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/product">Products</Link>
+                <Link to="/company">Companies</Link>
+            </nav>
         </div>
     );
 };
