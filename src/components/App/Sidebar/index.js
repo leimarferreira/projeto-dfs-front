b7b2dee5bc006f59ec64979b77557709c3e3f41a
Leimar <leimarferreira100@gmail.com>
2021-04-20 08:45:41 -0300

    Adding a header and a sidebar to the app.

---
+++ src/components/App/Sidebar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+
+        </div>
+    );
+};
+
+export default Sidebar;
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Authentication from '../Authentication/index';
+import Header from './Header/index'
+import Sidebar from './Sidebar/index';
 
 function App() {
     return (
-        <Switch>
-            <Route exact path={"/"} component={ Authentication }/>
-        </Switch>
+        <div>
+            <Header/>
+            <Sidebar/>
+        </div>
     )
 }
 
