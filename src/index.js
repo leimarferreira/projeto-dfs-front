48bb135c13d173d7eaf3dd54fe044b4be3e7769a
Leimar <leimarferreira100@gmail.com>
2021-04-20 09:54:12 -0300

    Updating index.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './components/App/index';
+import Authentication from './components/Authentication';
 
 ReactDOM.render(
     <BrowserRouter>
-        <App/>
+        <Switch>
+            <Route exact path={["/", "/signin", "/signup"]}>
+                <Authentication/>
+            </Route>
+            <Route exact path={"/product"}>
+                <App/>
+            </Route>
+        </Switch>
     </BrowserRouter>,
     document.getElementById('root')
 );
