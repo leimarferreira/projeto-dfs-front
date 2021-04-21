82ccadca4f8d93a01616334305dc4cd91ed2e2e0
Leimar <leimarferreira100@gmail.com>
2021-04-21 08:35:38 -0300

    Adding a product page.

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,6 +1,39 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import ProductDataService from '../../services/Product/index';
 
 const Product = () => {
-    
-}
+
+    const [product, setProduct] = useState({});
+    const { id } = useParams();
+    useEffect(() => {
+        retrieveProduct(id);
+    });
+
+    const retrieveProduct = id => {
+        ProductDataService.get(id)
+            .then(response => {
+                setProduct(response.data);
+            })
+            .catch(error => {
+                // TODO: ...
+            })
+    }
+
+    return (
+        <div>
+            <div>
+                <p>{ product.name }</p>
+                <p>{ product.description }</p>
+                <p>{ product.value }</p>
+                <p>{ product.note }</p>
+            </div>
+
+            <button>
+                Comprar
+            </button>
+        </div>
+    );
+};
+
+export default Product;
