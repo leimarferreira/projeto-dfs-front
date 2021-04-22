a562fffcf2e75d722a374b85d53a2e680bacee3e
Leimar <leimarferreira100@gmail.com>
2021-04-22 09:11:03 -0300

    Updating the header.

--- src/components/App/Header/index.js
+++ src/components/App/Header/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import './index.css';
+import { Link } from 'react-router-dom';
+//import './index.css';
 
 const Header = () => {
     return (
-        <div className="header">
-
+        <div className="navbar navbar-expand-xl bg-dark fixed-top">
+            <ul className="navbar-nav nav-pills">
+                <li className="nav-item">
+                    <Link to="/product" className="nav-link">Products</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/company" className="nav-link">Companies</Link>
+                </li>
+            </ul>
         </div>
-    )
-}
+    );
+};
 
 export default Header;
