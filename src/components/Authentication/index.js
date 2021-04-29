963a5886cb144e22af5bfeafc3c1d97c302b7634
Leimar <leimarferreira100@gmail.com>
2021-04-29 20:04:57 -0300

    Updating authentication screen.

--- src/components/Authentication/index.js
+++ src/components/Authentication/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import SignIn from './SignIn/index';
 import SignUp from './SignUp/index';
+import './index.css';
 
 const Authentication = () => {
     const [containerSize, setContainerSize] = useState('w-100');
@@ -25,20 +26,22 @@ const Authentication = () => {
     }
 
     return (
-        <div className={`container ${containerSize}`}>
-            <ul className="nav nav-tabs nav-justified">
-                <li className="nav-item">
-                    <Link to="/signin" className="nav-link">Sign in</Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/signup" className="nav-link">Sign up</Link>
-                </li>
-            </ul>
+        <div className="vertical-center">
+            <div className={`container ${containerSize} border rounded bg-light p-3`}>
+                <ul className="nav nav-tabs nav-justified">
+                    <li className="nav-item">
+                        <Link to="/signin" className="nav-link">Sign in</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/signup" className="nav-link">Sign up</Link>
+                    </li>
+                </ul>
 
-            <Switch>
-                <Route exact path={["/", "/signin"]} component={SignIn} />
-                <Route exact path={"/signup"} component={SignUp} />
-            </Switch>
+                <Switch>
+                    <Route exact path={["/", "/signin"]} component={SignIn} />
+                    <Route exact path={"/signup"} component={SignUp} />
+                </Switch>
+            </div>
         </div>
     );
 };
